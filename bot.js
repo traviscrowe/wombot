f587f7dddaafd9ea228dfaf8679e9042708cbe5a
Travis Crowe <traviscrowe@gmail.com>
2017-11-14 14:12:12 -0600

    feat: re-wrote bgg search to replace preview with more compact and usefule metadata; eslint fixes; updated docs

--- bot.js
+++ bot.js
@@ -1,14 +1,11 @@
 const config = require('./config.json');
-const util = require('./util.js');
-const request = require('request');
-const xmlParser = require('xml2js').parseString;
 const Discord = require('discord.js');
 const messageHandler = require('./messageHandler.js');
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.channels.find('name', 'wombot-testing').send('*wombat noises* [wombot started]');
+    //client.channels.find('name', 'wombot-testing').send('*wombat noises* [wombot started]');
 });
 
 client.on('message', (msg) => {
