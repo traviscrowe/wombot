a3063daeb143b5a67fb7cd68fb04ef011682a4b3
Travis Crowe <traviscrowe@gmail.com>
2017-11-14 14:39:54 -0600

    Revert "feat: re-wrote bgg search to replace preview with more compact and usefule metadata; eslint fixes; updated docs"
    This reverts commit f587f7dddaafd9ea228dfaf8679e9042708cbe5a.

--- test/messageHandler.spec.js
+++ test/messageHandler.spec.js
@@ -9,13 +9,13 @@ function createMessage(channel, messageText, sendCallback) {
             name: channel,
             send: sendCallback
         }
-    };
+    }
 }
 
 describe('MessageHandler', () => {
     describe('Reddit Helper', () => {
-        it('Returns Reddit Links', (done) => {
-            messageHandler.handle(createMessage('any', 'something about /r/btc', (text) => {
+        it('Returns Reddit Links', done => {
+            messageHandler.handle(createMessage('any', 'something about /r/btc', text => {
                 expect(text).to.equal('https://reddit.com/r/btc');
                 done();
             }));
@@ -23,15 +23,15 @@ describe('MessageHandler', () => {
     });
 
     describe('Die Roll', () => {
-        it('Rolls 1d6', (done) => {
-            messageHandler.handle(createMessage('any', 'please .roll 1d6', (text) => {
+        it('Rolls 1d6', done => {
+            messageHandler.handle(createMessage('any', 'please .roll 1d6', text => {
                 expect(text).to.match(/You rolled \d/);
                 done();
             }));
         });
 
-        it('Rolls 5d20', (done) => {
-            messageHandler.handle(createMessage('any', '.roll 5d20 now', (text) => {
+        it('Rolls 5d20', done => {
+            messageHandler.handle(createMessage('any', '.roll 5d20 now', text => {
                 expect(text).to.match(/You rolled \d+, \d+, \d+, \d+, \d/);
                 done();
             }));
