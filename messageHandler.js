7dc93efbddbcba48ad355844684316ea2c405584
root <root@ip-172-31-93-131.ec2.internal>
2017-11-14 21:52:50 +0000

    Revert "Revert "feat: re-wrote bgg search to replace preview with more compact and usefule metadata; eslint fixes; updated docs""
    This reverts commit 64c18c5fbd5bdd16e09c88935b5c98e51723d656.

--- messageHandler.js
+++ messageHandler.js
@@ -1,6 +1,7 @@
+const options = {};
 const util = require('./util.js');
-const request = require('request');
-const xmlParser = require('xml2js').parseString;
+const bgg = require('bgg')(options);
+const ba = require('beeradvocate-api');
 
 module.exports = {
     handle: (msg) => {
@@ -13,6 +14,34 @@ module.exports = {
             });
         }
 
+        const dieRollQuery = msg.content.match(/\.roll (\d+)d(\d+)/);
+        if (dieRollQuery) {
+            const numDice = parseInt(dieRollQuery[1], 10);
+            const numPips = parseInt(dieRollQuery[2], 10);
+
+            if (numDice === 0 || numPips === 0) {
+                msg.channel.send('wat?');
+                return;
+            }
+
+            if (numDice > 100) {
+                msg.channel.send('That\'s a lot of dice!');
+                return;
+            }
+
+            let result = 'You rolled ';
+
+            for (let i = 0; i < numDice; i += 1) {
+                if (i > 0) {
+                    result += ', ';
+                }
+
+                result += Math.floor(Math.random() * numPips) + 1;
+            }
+
+            msg.channel.send(result);
+        }
+
         if (msg.channel.name === 'happy-hour' || msg.channel.name === 'wombot-testing') {
             if (msg.content.substring(0, 5) === '🍻 @ ') {
                 const mapsQuery = util.encodeForQs(msg.content.substring(5, msg.content.length));
@@ -21,15 +50,13 @@ module.exports = {
 
             const beerQuery = msg.content.match(/(?:\.beer )(\w.*)/);
             if (beerQuery) {
-                var ba = require('beeradvocate-api');
-                var beerResult = '';
-                ba.beerSearch(beerQuery[1], function(beers) {
-                    var beersArray = JSON.parse(beers);
-                    for (var beer in beersArray) {
-                        var b = beersArray[beer];
-                        beerResult += b.beer_name + ' - ' + '<https://www.beeradvocate.com' + b.beer_url + '>' + '\n';
-                    };
-                    var safeBeerResult = beerResult.substring(0, 1999) //can only send 2000 char messages to discord
+                let beerResult = '';
+                ba.beerSearch(beerQuery[1], (beers) => {
+                    const beersArray = JSON.parse(beers);
+                    beersArray.forEach((beer) => {
+                        beerResult += `${beer.beer_name} - <https://www.beeradvocate.com${beer.beer_url}>\n`;
+                    });
+                    const safeBeerResult = beerResult.substring(0, 1999); // can only send 2000 char messages to discord
                     msg.channel.send(safeBeerResult);
                 });
             }
@@ -48,59 +75,39 @@ module.exports = {
         if (msg.channel.name === 'board-games' || msg.channel.name === 'wombot-testing') {
             const games = msg.content.match(/\{\{(.*?)\}\}/g);
             if (games) {
-                games.forEach((game) => {
-                    const options = {
-                        url: `https://www.boardgamegeek.com/xmlapi2/search?type=boardgame&query=${game}`,
-                        headers: {
-                            Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                        },
-                    };
-                    request(options, (error, response, body) => {
-                        if (error) {
-                            msg.channel.send('sorry, something is fucked');
-                        }
-                        return xmlParser(body, (err, result) => {
-                            if (err) {
+                games.forEach((gameQuery) => {
+                    bgg('search', { type: 'boardgame', query: gameQuery.toLowerCase() }).then((results) => {
+                        if (!results.items.total) {
+                            msg.channel.send('no results!');
+                        } else {
+                            bgg('thing', { thingtype: 'boardgame', id: results.items.item[0].id, stats: 1 }).then((result) => {
+                                if (!result) {
+                                    msg.channel.send('no results!');
+                                } else {
+                                    const game = result.items.item;
+                                    const regexp = /.+?(?=&amp;&amp;#35)/;
+                                    const name = game.name.length > 1 ? game.name[0].value : game.name.value;
+                                    const players = game.minplayers.value === game.maxplayers.value ?
+                                        game.minplayers.value : `${game.minplayers.value} - ${game.maxplayers.value}`;
+                                    const minutes = game.minplaytime.value === game.maxplaytime.value ?
+                                        game.minplaytime.value : `${game.minplaytime.value} - ${game.maxplaytime.value}`;
+                                    msg.channel.send(`${name} [${game.yearpublished.value}]\n` +
+                                    `${players} players\n` +
+                                    `${minutes} minutes\n` +
+                                    `${game.statistics.ratings.average.value} avg. rating\n` +
+                                    `<https://www.boardgamegeek.com/boardgame/${game.id}>\n` +
+                                    `${game.description.match(regexp)}\n` +
+                                    `${game.thumbnail}\n`);
+                                }
+                            }, () => {
                                 msg.channel.send('sorry, something is fucked');
-                            }
-                            if (parseInt(result.items.$.total, 10) > 0) {
-                                const bestResult = result.items.item[0];
-                                msg.channel.send(`\`${bestResult.name[0].$.value} [${bestResult.yearpublished[0].$.value}]\`\nhttps://www.boardgamegeek.com/boardgame/${bestResult.$.id}`);
-                            } else {
-                                msg.channel.send('no results!');
-                            }
-                        });
+                            });
+                        }
+                    }, () => {
+                        msg.channel.send('sorry, something is fucked');
                     });
                 });
             }
-        }
-
-        const dieRollQuery = msg.content.match(/\.roll (\d+)d(\d+)/);
-        if (dieRollQuery) {
-            const numDice = parseInt(dieRollQuery[1]);
-            const numPips = parseInt(dieRollQuery[2]);
-
-            if (numDice === 0 || numPips === 0) {
-                msg.channel.send('wat?');
-                return;
-            }
-
-            if (numDice > 100) {
-                msg.channel.send('That\'s a lot of dice!');
-                return;
-            }
-
-            let result = "You rolled ";
-
-            for (let i = 0; i < numDice; i++) {
-                if (i > 0) {
-                    result += ", ";
-                }
-
-                result += Math.floor(Math.random() * numPips) + 1;
-            }
-
-            msg.channel.send(result);
         }
 
         if (msg.channel.name === 'wombot' || msg.channel.name === 'wombot-testing') {
@@ -110,16 +117,16 @@ module.exports = {
 
             if (msg.content === 'wb.docs') {
                 msg.channel.send('```womdocs:\n\n' +
-                            'global:\n\n' +
-                            '    - messages with subreddit references (\'/r/\') will be responded to with a link to the subreddit\n' +
-                            '    - use .roll xdy to roll x dice with y pips\n\n' +
-                            '#happy-hour:\n\n' +
-                            '    - messages beginning with \'🍻 @ \' will return a Google Maps URL searching for whatever is after the prefix\n\n' +
-                            '#board-games:\n\n' +
-                            '    - board game titles surrounded by double braces, like {{this}} will return a BGG URL searching for the game\n\n' +
-                            '#wallstreetbets:\n\n' +
-                            '    - stock tickers referenced like $NAK will return a Google Finance URL to fetch the daily chart of that ticker\n\n' +
-                            'Questions? Submit a PR and fix it yourself at https://github.com/traviscrowe/wombat```');
+                    'global:\n\n' +
+                    '    - use /r/something to get a link to the subreddit\n' +
+                    '    - use .roll xdy to roll x dice with y pips\n\n' +
+                    '#happy-hour:\n\n' +
+                    '    - use \'🍻 @ \' followed by the name of a location to get a Google Maps URL pointed to that location\n\n' +
+                    '#board-games:\n\n' +
+                    '    - {{BoardGameTitle}} in messages will return a bunch of information about and a link to the best match of BoardGameTitle on BoardGameGeek\n\n' +
+                    '#wallstreetbets:\n\n' +
+                    '    - $XYZ in messages will return a Google Finance URL to fetch the daily chart of XYZ ticker\n\n' +
+                    'Questions? Submit a PR and fix it yourself at https://github.com/traviscrowe/wombat```');
             }
         }
     }
